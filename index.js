59fc0abaf41f303c78a1ed9a32f842cf97709708
rhythmdev <rhythmbiswas@gmail.com>
2022-10-26 12:28:37 +0600

    course acess by dynamic id done

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors')
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
-const course = require('./data/course.json')
+const course = require("./data/course.json");
 
-app.get('/', (req, res) => {
-    res.send("Think Showcase Api Running")
-})
+app.get("/", (req, res) => {
+  res.send("Think Showcase Api Running");
+});
 
-app.get('/courses', (req, res) => {
-    res.send(course)
-})
+app.get("/courses", (req, res) => {
+  res.send(course);
+});
+
+app.get("/course/:id", (req, res) => {
+  const id = req.params.id;
+
+  const singleCourse = course.find((c) => c.id == id);
+  res.send(singleCourse);
+});
 
 app.listen(port, () => {
-console.log('Think Showcase Server Running')
-})
+  console.log("Think Showcase Server Running");
+});
