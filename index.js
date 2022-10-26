8d1a436f9005edbcf43dcd680422173809600bcc
rhythmdev <rhythmbiswas@gmail.com>
2022-10-26 12:15:47 +0600

    all courses data path acess link working

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const app = express();
 const cors = require('cors')
 const port = process.env.PORT || 5000;
 
+const course = require('./data/course.json')
 
 app.get('/', (req, res) => {
     res.send("Think Showcase Api Running")
+})
+
+app.get('/courses', (req, res) => {
+    res.send(course)
 })
 
 app.listen(port, () => {
